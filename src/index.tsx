ea4b6ecf0e96c036fe3bff3f599b228969c1d3ec
CarlosSuarezJS20 <69536882+CarlosSuarezJS20@users.noreply.github.com>
2021-07-14 15:41:43 +0100

    added last changes and re-hosted

--- src/index.tsx
+++ src/index.tsx
@@ -6,6 +6,15 @@ import reportWebVitals from "./reportWebVitals";
 import { BrowserRouter } from "react-router-dom";
 import ScrollToTop from "./components/UI/scrollToTop/ScrollToTop";
 
+// @ts-ignore
+import TagManager from "react-gtm-module";
+
+const tagManagerArgs = {
+  gtmId: "GTM-KKB455T",
+};
+
+TagManager.initialize(tagManagerArgs);
+
 ReactDOM.render(
   <BrowserRouter>
     <ScrollToTop />
